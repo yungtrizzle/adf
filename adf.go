383d6fb620a517a7e9edf3864b27534ca5b212b4
Eshton Robateau <eshtonrob@gmail.com>
2020-10-03 23:18:56 +0800

    Update adf.go

--- adf.go
+++ adf.go
@@ -4,9 +4,8 @@
 package adf
 
 import (
-	"github.com/berkmancenter/ridge"
-	"github.com/gonum/matrix/mat64"
-	"github.com/gonum/stat"
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
 
 	"math"
 )
@@ -25,15 +24,18 @@ type ADF struct {
 }
 
 // New creates and returns a new ADF test.
-func New(series []float64, pvalue float64, lag int) *ADF {
+func NewADF(series []float64, pvalue float64, lag int) *ADF {
 	if pvalue == 0 {
 		pvalue = DefaultPValue
 	}
+
 	if lag < 0 {
 		lag = int(math.Floor(math.Cbrt(float64(len(series)))))
 	}
+
 	newSeries := make([]float64, len(series))
 	copy(newSeries, series)
+
 	return &ADF{Series: newSeries, PValueThreshold: pvalue, Lag: lag}
 }
 
@@ -41,38 +43,47 @@ func New(series []float64, pvalue float64, lag int) *ADF {
 func (adf *ADF) Run() {
 	series := adf.Series
 	mean := stat.Mean(series, nil)
+
 	if mean != 0.0 {
 		for i, v := range series {
 			series[i] = v - mean
 		}
 	}
+
 	n := len(series) - 1
 	y := diff(series)
 	lag := adf.Lag
 	k := lag + 1
+
 	z := laggedMatrix(y, k)
-	zcol1 := mat64.Col(nil, 0, z)
+
+	zcol1 := mat.Col(nil, 0, z)
 	xt1 := series[k-1 : n]
 	r, c := z.Dims()
-	var design *mat64.Dense
+
+	var design *mat.Dense
 
 	if k > 1 {
-		yt1 := z.View(0, 1, r, c-1)
-		design = mat64.NewDense(n-k+1, k, nil)
+		yt1 := view(z, 0, 1, r, c-1)
+		design = mat.NewDense(n-k+1, k, nil)
 		design.SetCol(0, xt1)
 
 		_, c = yt1.Dims()
+
 		for i := 0; i < c; i++ {
-			design.SetCol(1+i, mat64.Col(nil, i, yt1))
+			design.SetCol(1+i, mat.Col(nil, i, yt1))
 		}
+
 	} else {
-		design = mat64.NewDense(n-k+1, 1, nil)
+		design = mat.NewDense(n-k+1, 1, nil)
 		design.SetCol(0, xt1)
 	}
-	regressY := mat64.NewVector(len(zcol1), zcol1)
 
-	rr := ridge.New(design, regressY, LPenalty)
+	regressY := mat.NewVecDense(len(zcol1), zcol1)
+
+	rr := NewRidge(design, regressY, LPenalty)
 	rr.Regress()
+
 	beta := rr.Coefficients.RawVector().Data
 	sd := rr.StdErrs
 
@@ -92,13 +103,18 @@ func diff(x []float64) []float64 {
 	return y
 }
 
-func laggedMatrix(series []float64, lag int) *mat64.Dense {
+func laggedMatrix(series []float64, lag int) *mat.Dense {
 	r, c := len(series)-lag+1, lag
-	m := mat64.NewDense(r, c, nil)
+	m := mat.NewDense(r, c, nil)
+
 	for j := 0; j < c; j++ {
 		for i := 0; i < r; i++ {
 			m.Set(i, j, series[lag-j-1+i])
 		}
 	}
 	return m
+}
+
+func view(m *mat.Dense, i, j, r, c int) mat.Matrix {
+	return m.Slice(i, i+r, j, j+c)
 }
